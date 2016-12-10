d0055023b34456f0c7b87f6761f161d4c782e639
hyunjune.kim <hyunjune@chromium.org>
2016-12-11 03:19:01 +0900

    Enable DUK_USE_REPL_FPCLASSIFY

--- cmake/duktape.cmake
+++ cmake/duktape.cmake
@@ -4,5 +4,6 @@ set(ROOT ${CMAKE_SOURCE_DIR})
 file(GLOB LIB_DUKTAPE_SRC ${ROOT}/*.c)
 set(INCLUDE_PATH ${ROOT})
 
+add_definitions(-DDUK_USE_REPL_FPCLASSIFY)
 set(targetName duktape)
 add_library(${targetName} ${LIB_DUKTAPE_SRC})
